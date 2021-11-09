e1e698d9c58993ef42019bd0a29ad9a05dbe679f
sukeun1997 <busan-sy@naver.com>
2021-11-09 15:46:47 +0900

    CLN : AccountController

--- src/main/java/com/studyforyou/account/AccountController.java
+++ src/main/java/com/studyforyou/account/AccountController.java
@@ -22,8 +22,7 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -44,24 +43,7 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO encoding 해야함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디포유, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token="+newAccount.getEmailCheckToken()+"&email="+newAccount.getEmail());
-
-        javaMailSender.send(mailMessage);
+        accountService.processNewAccount(signUpForm);
         // TODO 회원 가입 처리
         return "redirect:/";
     }
